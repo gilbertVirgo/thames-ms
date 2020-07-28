071900d32562f1a92fee33326f51d9e88458d927
Gil <gilbertjvirgo@gmail.com>
2020-07-28 18:37:16 +0100

    Added a nav component and a login page to be worked on

---
+++ app/src/components/Nav.js
@@ -0,0 +1,30 @@
+import Button from "react-bootstrap/Button";
+import { ReactComponent as Home } from "../assets/icons/home.svg";
+import Navbar from "react-bootstrap/Navbar";
+import React from "react";
+import theme from "../theme";
+import { useHistory } from "react-router-dom";
+
+export default () => {
+	const history = useHistory();
+
+	return (
+		<Navbar
+			bg="light"
+			expand="lg"
+			style={{ marginBottom: theme.gutter + "px" }}
+		>
+			<Home
+				style={{ width: "30px", height: "30px" }}
+				onClick={() => history.push("/")}
+			/>
+			{/* <Navbar.Toggle aria-controls="basic-navbar-nav" />
+			<Navbar.Collapse id="basic-navbar-nav">
+				<Nav className="mr-auto">
+					<Nav.Link href="#home">Home</Nav.Link>
+					<Nav.Link href="#link">Link</Nav.Link>
+				</Nav>
+			</Navbar.Collapse> */}
+		</Navbar>
+	);
+};
---
+++ app/src/pages/Login.js
@@ -0,0 +1,8 @@
+import GoogleButton from "react-google-button";
+import React from "react";
+
+const Login = () => {
+	return <p>Hi</p>;
+};
+
+export default Login;
