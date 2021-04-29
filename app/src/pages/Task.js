203d7eb53358e21d2acfabe714d03930097c9865
inkarbegim <inkarbegimkereyeva@gmail.com>
2021-04-29 12:53:04 +0100

    new page for each tasks in the list

---
+++ app/src/pages/Task.js
@@ -0,0 +1,13 @@
+import React from "react";
+import CountDateButton from "../components/CountDateButton";
+import ListItem from "../components/ListItem";
+import styled from "styled-components";
+import Menu from "../components/Menu";
+
+export default () => {
+	return (
+		<React.Fragment>
+            <Menu />
+		</React.Fragment>
+	);
+};
