6a3eb9f2b27b8f469d661d6d2da76584ef3ef5a6
inkarbegim <inkarbegimkereyeva@gmail.com>
2021-04-29 14:36:01 +0100

    task page added

---
+++ app/src/components/TaskContent.js
@@ -0,0 +1,79 @@
+import React from "react";
+import styled from "styled-components";
+import {Grid} from "./index";
+
+const Wrapper = styled.div`
+	box-sizing: border-box;
+    padding: 0;
+    padding: 19px 30px;
+
+    width:100vw;
+    height: 100%;
+
+    background: #F8F8F8;
+`;
+
+const Content = styled.div`
+    font-weight: 800;
+    font-size: 15px;
+    line-height: 20px;
+
+    color: #4E4E4E;
+
+    margin: 0;
+    padding: 0;
+
+    h1,h2,h3,h4,h5,h6{
+
+    }
+
+    p{
+        font-weight: 600;
+    }
+
+    input{
+
+    }
+
+`;
+
+
+const Attachment = styled.div`
+
+`;
+
+const CompleteToggle = styled.div`
+    margin-top: 38px;
+    padding: 18px 23px;
+    width: 100%;
+    height: 62px;
+
+    border-radius: 3px;
+    background: #FFFFFF;
+    border: 3px solid #EAAA00;
+
+    color: #002E5D;
+    font-weight: 800;
+    font-size: 15px;
+    line-height: 20px;
+`;
+
+const ToogleImage = styled.img`
+
+`;
+
+const TaskContent = ({content}) => {
+	return (
+		<Wrapper>
+            <Content>
+                {content}
+                
+            </Content>
+		    <CompleteToggle>
+                 Have you completed the task?   
+            </CompleteToggle>
+		</Wrapper>
+	);
+};
+
+export default TaskContent;
--- app/src/pages/Task.js
+++ app/src/pages/Task.js
@@ -1,12 +1,12 @@
 import React from "react";
-import CountDateButton from "../components/CountDateButton";
-import ListItem from "../components/ListItem";
 import styled from "styled-components";
 import Menu from "../components/Menu";
+import TaskContent from "../components/TaskContent";
 
 export default () => {
 	return (
 		<React.Fragment>
+            <TaskContent />
             <Menu />
 		</React.Fragment>
 	);
