7eabddee4fe980cee6b691de3aacf9279a8f761f
inkarbegim <48454291+inkarbegim@users.noreply.github.com>
2021-06-11 09:37:50 +0100

    profile pgae itself

--- app/src/pages/Profile.js
+++ app/src/pages/Profile.js
@@ -1,97 +1,66 @@
 import API from "../api";
 import Menu from "../components/Menu";
 import React from "react";
-import ReviewAssignment from "./sections/ReviewAssignment";
-import ProfileContent from "../components/ProfileContent";
-import TaskHeader from "../components/TaskHeader";
+import ProfileInfo from "../components/ProfileInfo";
 import ProfileHeader from "../components/ProfileHeader";
+import ProfilePoints from "../components/ProfilePoints";
+import ProfileCommendations from "../components/ProfileCommendations";
+import ProfileContent from "../components/ProfileContent";
+
 import cheerio from "cheerio";
 import moment from "moment";
 import { useParams } from "react-router-dom";
 import useRole from "../hooks/useRole";
 
 export default () => {
-	// const [role] = useRole();
+	const [role] = useRole();
+	const { id } = useParams();
+	const [record, setRecord] = React.useState(null);
+	const [loading, setLoading] = React.useState(true);
+	const [error, setError] = React.useState();
+	
 
-	// const { id } = useParams();
-	// const [record, setRecord] = React.useState(null);
-	// const [loading, setLoading] = React.useState("Loading assignment data...");
-	// const [error, setError] = React.useState();
-	// const [content, setContent] = React.useState("");
-	// const [studentCompleted, setStudentCompleted] = React.useState();
-	// const [reviewId, setReviewId] = React.useState();
+	React.useEffect(() => {
+		if (loading) {
+			(async function () {
+				try {
+					const response = await API.get(`student/${id}`);
 
-	// const translateDate = (date) => {
-	// 	return moment(new Date(date)).format("MMM Do YY");
-	// };
+					console.log("student call successful");
 
-	// const translateDatetoWeek = (date) => {
-	// 	return moment(new Date(date)).format("dddd");
-	// };
+					if (!response.hasOwnProperty("content"))
+						throw new Error("Empty response");
 
-	// const parseContent = (content) => {
-	// 	const $ = cheerio.load(content);
+					console.log(response);
 
-	// 	$("a").prepend(
-	// 		`<h1>Attachments</h1><img src='${require("../assets/icons/paperclip.svg")}' />`
-	// 	);
-
-	// 	return $.html()
-	// 		.replace("<html><head></head><body>", "")
-	// 		.replace("</body></html>", "");
-	// };
-
-	// React.useEffect(() => {
-	// 	if (loading) {
-	// 		(async function () {
-	// 			try {
-	// 				const response = await API.get(`assignment/${id}`);
-
-	// 				console.log("Assignment call successful");
-
-	// 				if (!response.hasOwnProperty("content"))
-	// 					throw new Error("Empty response");
-
-	// 				const record = response.content[0].fields;
-	// 				const {
-	// 					content: [
-	// 						{
-	// 							fields: { Student_Checked, id: reviewId },
-	// 						},
-	// 					],
-	// 				} = await API.get(`reviews?assignment_id=${id}`);
-
-	// 				setReviewId(reviewId);
-	// 				setStudentCompleted(Student_Checked);
-	// 				setRecord(record);
-	// 				setContent(parseContent(record.Content));
-	// 				setLoading(false);
-	// 			} catch (err) {
-	// 				setError(err.toString());
-	// 			}
-	// 		})();
-	// 	}
-	// }, [loading]);
-
-	// const handleCompletedChange = async () => {
-	// 	await API.update(`review/${reviewId}`, {
-	// 		Student_Checked: !studentCompleted,
-	// 	});
-
-	// 	setStudentCompleted(!studentCompleted);
-	// };
+					setRecord(response.content[0].fields);
+					console.log("Record is", record);
+					setLoading(false);
+				} catch (err) {
+					setError(err.toString());
+				}
+			})();
+		}
+	}, [loading]);
 
 	return(
 		<React.Fragment>
 			<ProfileHeader
-                name="James John Smith"
+                name="My name"
+				// {`${record.Forename} ${record.Surname}`}
                 pointssystem ="Commendations:"
                 points="125"
 			/>
-
-            <ProfileContent>
-
-            </ProfileContent>
+			<ProfilePoints points="125" />
+            <ProfileInfo 
+				year="5" //{record.Year_Group}
+				tutor="smone"
+				email="email.co.uk"
+			 />
+			<ProfileCommendations />
+			<ProfileContent 
+				achievement="James has shown great leadership qualities in DofE preperations. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Orci purus purus diam, gravida nunc accumsan odio eleifend. "
+			/>
             <Menu activeAssignment={false} activeAvatar={true}/>
 		</React.Fragment>
 	);
