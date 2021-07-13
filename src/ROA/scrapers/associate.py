343e9a344657cb863517f77989eb8d2f2e31a1aa
Oelin <me.oelin@gmail.com>
2021-07-13 11:55:21 +0100

    finished associate.py

--- src/ROA/scrapers/associate.py
+++ src/ROA/scrapers/associate.py
@@ -1,15 +1,34 @@
 # associate.py, a scraper uses thesaurus.com to associate words with university courses
 
 from courses import courses
-from reqeusts import get
-
+from requests import get
+from urllib.parse import quote, unquote
+from re import findall
 
 
 def words_related_to(course):
-  page = get(f'https://www.thesaurus.com/browse/{"+".join(course.split(" ")}')
+  page = get(f'https://www.thesaurus.com/browse/{quote(course)}').text
+  
+  # find all synonyms
 
+  syns = findall('"/browse/((\w+|%\d{2})+)" data-linkid="nn1ov4" class="css-1[^5]', page)
+
+  return [unquote(syn[0]) for syn in syns]
 
 
 def scrape():
+  print('[*] starting')
+  batch = []
+
   for course in courses:
-    pass
+    try:
+      batch[course] = words_related_to[course.lower()]
+    except:
+      print(f'[!] failed on {course}')
+
+  print('[*] finished')
+  print(batch)
+
+
+if __name__ == '__main__':
+  scrape()
